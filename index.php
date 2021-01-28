1396cd06d3a85b14f1ca5e5fdac7294642a4fad0
Andrea Iglio <andrea.iglio@gmail.com>
2021-01-28 17:41:26 +0100

    Completed project

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 $name = 'Andrea';
 $lastName = 'Iglio';
+$second_name = $argv[1];
+$second_lastName = $argv[2];
+$testo = 'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, mollitia!';
+$var_get = $_GET['badwords'];
 ?>
 
 
@@ -14,9 +18,15 @@ $lastName = 'Iglio';
 </head>
 <body>
     
+    <h1>Ciao mi chiamo <?php echo $var_get . " " . $lastName ?></h1>
 
-    <h1>Ciao mi chiamo <?php echo $name . ' ' . $lastName ?></h1>
+    <h1>Ciao mi chiamo <?php echo $second_name . " " . $second_lastName  ?></h1>
+    <h5><?php  echo strlen($testo)   ?></h5>
+    <h6><?php echo strpos($testo, 'sit') ?></h6>
+    <h6><?php echo str_replace($_GET, "***" ,$testo)?></h6>
 
+    <p> <?php echo $testo ?> </p>
+    <p> <?php echo str_replace($var_get, "***" ,$testo)?></p> 
 
 </body>
 </html>
